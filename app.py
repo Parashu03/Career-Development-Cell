1e33c8da9ab203d5b29aa17ad0e7e2f28c5d31de
Parashu03 <gattiparashu03@gamil.com>
2025-08-17 11:26:29 +0530

    created static and template

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ client = MongoClient('mongodb://localhost:27017')
 db = client['CDC']  
 collection = db['Student_details'] 
   
-
+@app.route("/user/<username>")
+def user_profile(username):
+    user = db.collection.find_one_or_404({"_id": username})
+    return render_template("student_dashboard.html",user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
